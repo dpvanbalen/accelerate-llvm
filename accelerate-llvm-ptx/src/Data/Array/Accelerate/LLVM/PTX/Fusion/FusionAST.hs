0ba6ac622a323a4e0e2d69930b4a331fc8a4d1a6
David <d.p.vanbalen@uu.nl>
2021-08-26 12:00:21 +0200

    Demonstrate codegen directly on the new AST, much cleaner! Don't even need weakenings here now

--- accelerate-llvm-ptx/src/Data/Array/Accelerate/LLVM/PTX/Fusion/FusionAST.hs
+++ accelerate-llvm-ptx/src/Data/Array/Accelerate/LLVM/PTX/Fusion/FusionAST.hs
@@ -15,99 +15,6 @@ import Prelude hiding (take)
 import Data.Bifunctor
 import Data.Array.Accelerate.LLVM.PTX.StuffFromAccNewPipeline
 
-data FusedAcc i o where
-  Leaf :: (i -> CodeGen PTX o) -> FusedAcc i o
-
-  Branch :: Combine' li lo ri ro ti to
-         -> FusedAcc li lo
-         -> FusedAcc       ri ro
-         -> FusedAcc             ti to
-
--- | Defunctionalised weakening for tuplists.
-data (:>) big small where
-  End  ::           ()     :> ()
-  Toss :: b :> s -> (b, x) :>  s
-  Keep :: b :> s -> (b, x) :> (s, x)
-
-
--- `a` is always an "Operands x" here
-data Combine' leftI leftO rightI rightO totalI totalO where
-  Combine' :: Combine' () () () () () ()
-
-  Vertical'   :: Take a loa lo -> Take a ria ri
-              -> Combine' li  lo  ri  ro  ti  to
-              -> Combine' li  loa ria ro  ti  to
-
-  Diagonal'   :: Take a loa lo -> Take a ria ri -> Take a toa to
-              -> Combine' li  lo  ri  ro  ti  to
-              -> Combine' li  loa ria ro  ti  toa
-
-  Horizontal' :: Take a lia li -> Take a ria ri -> Take a tia ti
-              -> Combine' li  lo  ri  ro  ti  to
-              -> Combine' lia lo  ria ro  tia to
-
-  -- this one is weird: do you pick the left or the right output? 
-  -- probably change Combine' in acc/new-pipeline to exclude it
-  -- HorizontalO :: Combine' li  lo    ri  ro    ti  to
-  --             -> Combine' li (lo,a) ri (ro,a) ti (to,a)
-
-  WeakRightI :: Take a lia li -> Take a tia ti
-             -> Combine' li  lo  ri  ro  ti  to
-             -> Combine' lia lo  ri  ro  tia to
-  WeakRightO :: Take a loa lo -> Take a toa to
-             -> Combine' li  lo  ri  ro  ti  to
-             -> Combine' li  loa ri  ro  ti  toa
-
-  WeakLeftI :: Take a ria ri -> Take a tia ti
-            -> Combine' li  lo ri  ro  ti  to
-            -> Combine' li  lo ria ro  tia to
-  WeakLeftO :: Take a roa ro -> Take a toa to
-            -> Combine' li  lo ri  ro  ti  to
-            -> Combine' li  lo ri  roa ti  toa
-
--- the left input is a subset of the total input
-leftI :: Combine' li a b c ti d -> ti :> li
-leftI Combine'                 = End
-leftI (Vertical'   t1 t2    c) =                  leftI c
-leftI (Diagonal'   t1 t2 t3 c) =                  leftI c
-leftI (Horizontal' t1 t2 t3 c) = twoTakes t3 t1 $ leftI c
-leftI (WeakRightI  t1 t2    c) = twoTakes t2 t1 $ leftI c
-leftI (WeakRightO  t1 t2    c) =                  leftI c
-leftI (WeakLeftI   t1 t2    c) = fromTake t2    $ leftI c
-leftI (WeakLeftO   t1 t2    c) =                  leftI c
-
--- the right output is a subset of the total output
-rightO :: Combine' a b c ro d to -> to :> ro
-rightO Combine'                 = End
-rightO (Vertical'   t1 t2    c) =                  rightO c
-rightO (Diagonal'   t1 t2 t3 c) = fromTake t3    $ rightO c
-rightO (Horizontal' t1 t2 t3 c) =                  rightO c
-rightO (WeakRightI  t1 t2    c) =                  rightO c
-rightO (WeakRightO  t1 t2    c) = fromTake t2    $ rightO c
-rightO (WeakLeftI   t1 t2    c) =                  rightO c
-rightO (WeakLeftO   t1 t2    c) = twoTakes t2 t1 $ rightO c
-
--- the right input can be constructed from the total input and left output
-rightI :: Combine' a lo ri b ti c -> (lo -> ti -> ri)
-rightI Combine'                 = \() ()                   -> ()
-rightI (Vertical'   t1 t2    c) = \(take t1 -> (a, lo)) ti -> put t2 a (rightI c lo ti)
-rightI (Diagonal'   t1 t2 t3 c) = \(take t1 -> (a, lo)) ti -> put t2 a (rightI c lo ti)
-rightI (Horizontal' t1 t2 t3 c) = \lo (take t3 -> (a, ti)) -> put t2 a (rightI c lo ti)
-rightI (WeakRightI  t1 t2    c) = \lo (take t2 -> (_, ti)) ->           rightI c lo ti
-rightI (WeakRightO  t1 t2    c) = \(take t1 -> (_, lo)) ti ->           rightI c lo ti
-rightI (WeakLeftI   t1 t2    c) = \lo (take t2 -> (a, ti)) -> put t1 a (rightI c lo ti)
-rightI (WeakLeftO   t1 t2    c) = \lo ti                   ->           rightI c lo ti
-
--- the total output can be constructed from the other outputs
-totalO :: Combine' a lo b ro c to -> (lo -> ro -> to)
-totalO Combine'                 = \() ()      -> ()
-totalO (Vertical'   t1 t2    c) = \(take t1 -> (_, lo)) ro ->           totalO c lo ro
-totalO (Diagonal'   t1 t2 t3 c) = \(take t1 -> (a, lo)) ro -> put t3 a (totalO c lo ro)
-totalO (Horizontal' t1 t2 t3 c) = \lo ro                   ->           totalO c lo ro
-totalO (WeakRightI  t1 t2    c) = \lo ro                   ->           totalO c lo ro
-totalO (WeakRightO  t1 t2    c) = \(take t1 -> (a, lo)) ro -> put t2 a (totalO c lo ro)
-totalO (WeakLeftI   t1 t2    c) = \lo ro                   ->           totalO c lo ro
-totalO (WeakLeftO   t1 t2    c) = \lo (take t1 -> (a, ro)) -> put t2 a (totalO c lo ro)
 
 take :: Take a ab b -> ab -> (a, b)
 take Here (b, a) = (a, b)
@@ -117,52 +24,42 @@ put :: Take a ab b -> a -> b -> ab
 put Here a b = (b, a)
 put (There t) a (b, c) = (put t a b, c)
 
-fromTake :: Take a ab b -> b :> x -> ab :> x
-fromTake Here bx = Toss bx
-fromTake (There t) (Toss bx) = Toss (fromTake t bx)
-fromTake (There t) (Keep bx) = Keep (fromTake t bx)
-
-fromTake' :: Take a ab b -> x :> ab -> x :> b
-fromTake' t         (Toss x) = Toss (fromTake' t x)
-fromTake' Here      (Keep x) = Toss x
-fromTake' (There t) (Keep x) = Keep (fromTake' t x)
-
--- Can't really write this function, because the :> datatype encodes that not only is
--- c 'contained' in b, but also in the same order. The `Take`s don't respect this order.
--- despite being impossible, this seems like a very minor hurdle and just a sign of friction
--- between multiple attempts. A rewrite of one or two datatypes should fix this,
--- perhaps we simply put Take in :>'s constructors (making this function trivial).
-twoTakes :: Take a ab b -> Take a ac c -> b :> c -> ab :> ac
-twoTakes = undefined
-
-
-makeCombine :: LeftHandSideArgs body i scope
-            -> Combine' (ToIn body) (ToOut body) scope o i o
-makeCombine Base = _ -- bunch of WeakLeftO's on top of the base case, can't implement without a typeclass of 'tuples on top of unit'
-makeCombine (Reqr ta ta' lhsa) = Horizontal' Here ta' ta (makeCombine lhsa)
-makeCombine (Make ta lhsa) = Vertical' Here ta (makeCombine lhsa)
-makeCombine (Adju ta ta' lhsa) = undefined -- TODO mut
-makeCombine (EArg lhsa) = WeakLeftI Here Here (makeCombine lhsa)
-makeCombine (Ignr lhsa) = WeakLeftI Here Here (makeCombine lhsa)
-
-
-compile' :: ClusterAST op i o -> FusedAcc i o
-compile' None = Leaf pure
-compile' (Bind lhs op ast) = Branch (makeCombine lhs) (compileOp op) (compile' ast)
-
-compileOp :: op body -> FusedAcc (ToIn body) (ToOut body)
-compileOp = undefined -- implement all primitives
-
 type family ToIn  a where
   ToIn  ()              = ()
   ToIn  (In sh e  -> x) = (ToIn x, e)
   ToIn  (Mut sh e -> x) = (ToIn x, e)
+  ToIn  (Exp    e -> x) = (ToIn x, e)
   ToIn  (_ -> x)        =  ToIn x
 type family ToOut a where
   ToOut ()              = ()
   ToOut (Out sh e -> x) = (ToOut x, e)
   ToOut (Mut sh e -> x) = (ToOut x, e)
   ToOut (_  -> x)       =  ToOut x
+
+getIn :: LeftHandSideArgs body i o -> i -> ToIn body
+getIn Base () = ()
+getIn (Reqr t1 t2 lhs) i = let (x, i') = take t1 i in (getIn lhs i', x)
+getIn (Make t lhs) i = getIn lhs i
+getIn (Adju t1 t2 lhs) i = let (x, i') = take t1 i in (getIn lhs i', x)
+getIn (EArg lhs) (i, x) = (getIn lhs i, x)
+getIn (Ignr lhs) (i, x) =  getIn lhs i
+
+genOut :: LeftHandSideArgs body i o -> i -> ToOut body -> o
+genOut Base             ()    o     = 
+  ()
+genOut (Reqr t1 t2 lhs) i     o     = 
+  let (x, i') = take t1 i 
+  in put t2 x (genOut lhs i' o)
+genOut (Make t lhs)     i    (o, x) = 
+  put t x (genOut lhs i o)
+genOut (Adju t1 t2 lhs) i    (o, x) = 
+  let (y, i') = take t1 i 
+  in put t2 x (genOut lhs i' o)
+genOut (EArg lhs)      (i, x) o     =
+  (genOut lhs i o, x)
+genOut (Ignr lhs)      (i, x) o     =
+  (genOut lhs i o, x)
+
 
 -----------
 -- from acc/new-pipeline
--- accelerate-llvm-ptx/src/Data/Array/Accelerate/LLVM/PTX/Fusion/FusionCodeGen.hs
+++ accelerate-llvm-ptx/src/Data/Array/Accelerate/LLVM/PTX/Fusion/FusionCodeGen.hs
@@ -17,27 +17,15 @@ import Data.Array.Accelerate.LLVM.PTX.Fusion.FusionAST
 
 -- | This makes the 'body' of a kernel: still need to read all inputs from the arrays and write all outputs to them.
 -- 'inplace updates' are performed by simply assigning one array to be both input and output.
-compile :: FusedAcc aenv i o -> i -> CodeGen PTX o
-compile (Leaf f) i = f i
-
-compile (Branch c l r) totalIn = do
-  -- Within a kernel, we don't perform any loop-fusion anymore: this simply uses the 'leftI, rightI, totalO' combinators
-  -- to thread the CodeGen state through the sub-branches.
-  -- TODO: not doing loop fusion means we do some excess synchronisations (between warp-level and block-level scans/folds).
-  -- A middleground would be to split each fold/scan into two 'black boxes' (before and after sync), and interleave those
-  -- where possible (i.e. if there is no vertical dependancy)
-  let leftIn = leftI c $:> totalIn
-  leftOut <- compile l leftIn
-  let rightIn = rightI c leftOut totalIn
-  rightOut <- compile r rightIn
-  return $ totalO c leftOut rightOut
-
-
--- | Applying a weakening on a tuplist
--- (could just as easily write an equivalent for `Data.Array.Accelerate.AST.Environment.Val`)
-($:>) :: (a :> b) -> (a -> b)
-End      $:> x     = x
-(Toss w) $:> (y, _) =  w $:> y
-(Keep w) $:> (y, x) = (w $:> y, x)
-
+-- Within a kernel, we don't perform any loop-fusion anymore: this simply uses the 'leftI, rightI, totalO' combinators
+-- to thread the CodeGen state through the sub-branches.
+-- TODO: not doing loop fusion means we do some excess synchronisations (between warp-level and block-level scans/folds).
+-- A middleground would be to split each fold/scan into two 'black boxes' (before and after sync), and interleave those
+-- where possible (i.e. if there is no vertical dependancy)
+compile :: ClusterAST op i o -> i -> CodeGen PTX o
+compile None i = return i
+compile (Bind lhs op c) i = compileBody op (getIn lhs i) >>= compile c . genOut lhs i
+  where
+    compileBody :: op body -> ToIn body -> CodeGen PTX (ToOut body)
+    compileBody = _
 
